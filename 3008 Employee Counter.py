d8a98c08dc9d3183c9030f38aa21ea7c68cffc9d
Nater0214 <gamer.nater0214@gmail.com>
2021-12-07 17:45:52 -0500

    Updated Script

--- 3008 Employee Counter.py
+++ 3008 Employee Counter.py
@@ -1,11 +1,15 @@
+import statistics as stats
+
 def recalculate_vars(): #recalculate the variables
     global total
     global roaming
     global ratio
+    global rate
 
     total = 2 * (day + 1)
     roaming = total - trapped
     ratio = 100 * (trapped / total)
+    rate = stats.mean(log)
 
 
 print("Welcome to 3008 Employee Counter!") #greeting
@@ -21,33 +25,36 @@ if trapped < 0: #error checking
 if trapped > 2 * (day + 1):
     raise Exception("What the hell! You cant trap more than the amount in the store!")
 
+log = []
+for i in range(-1, day):
+    log.append(0)
+
 recalculate_vars()
 
-trends = []
-for i in range(-1, day):
-    trends.append(0)
-
 while True:
-    txt = "{} Employees Total\n{} Employees Trapped\n{} Employees Roaming\n{}% Trapped\n{} Employees Trapped/Lost Today\n"
-    print(txt.format(total, trapped, roaming, ratio, trends[day]))
+    txt = "{} Employees Total\n{} Employee(s) Trapped\n{} Employee(s) Roaming\n{}% Trapped\n{} Employee(s) Trapped/Lost Today\n{} Employee(s) trapped per day on average\n"
+    print(txt.format(total, trapped, roaming, round(ratio, 2), log[day], round(rate)))
     inp = input()
     if inp == "t":
         if roaming == 0:
             print("You got them all! How the fuck did you get another one?\n")
         else:
             trapped += 1
-            trends[day] += 1
+            log[day] += 1
             print("Nice job\n")
     elif inp == "l":
         if trapped == 0:
             print("You dont got any, how the fuck did you loose one?\n")
         else:
             trapped -= 1
-            trends[day] -= 1
+            log[day] -= 1
             print("Aw man!\n")
     elif inp == "d":
         day += 1
         print("Its a new day!\n")
-        trends.append(0)
+        log.append(0)
+    elif inp == "g":
+        print(log)
+        print()
 
     recalculate_vars()
